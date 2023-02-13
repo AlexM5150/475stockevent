eb32298a264bbf4eb2491b5abcf6bdfb4697ac56
Alex M <33159385+AlexM5150@users.noreply.github.com>
2023-02-13 14:56:52 -0800

    edit

--- stockbroker.cs
+++ stockbroker.cs
@@ -26,6 +26,7 @@ namespace Stock
 
         public string titles = "Broker".PadRight(16) + "Stock".PadRight(16) +
             "Value".PadRight(16) + "Changes".PadRight(20) + "Date and Time";
+
         Boolean flag = true;
 //---------------------------------------------------------------------------------------
  
@@ -54,7 +55,21 @@ namespace Stock
         ///     The eventhandler that raises the event of a change 
         /// </summary> 
         /// <param name="sender">The sender that indicated a change</param> 
-        /// <param name="e">Event arguments</param> 
+        /// <param name="e">Event arguments</param>
+        ///
+        void addTitles()
+        {
+            
+            if (flag)
+            {
+                using (StreamWriter outputFile = new StreamWriter(destPath))
+                {
+                    outputFile.WriteLine(titles);
+                }
+                flag = false;
+            }
+        }
+
         void EventHandler(Object sender, EventArgs e)
         {
             try
@@ -63,19 +78,12 @@ namespace Stock
                 Stock newStock = (Stock)sender;
                 //string statement; 
                 //!NOTE!: Check out C#events, pg.4 
-                // Display the output to the console windows      
-                string output = (BrokerName.PadRight(16) + newStock.StockName.PadRight(16) + newStock.CurrentValue.ToString().PadRight(16) +
-                    newStock.NumChanges.ToString().PadRight(16) + DateTime.Now.ToString().PadRight(16));
+                // Display the output to the console windows
+                string output = (BrokerName).PadRight(16) + newStock.StockName.PadRight(16) + newStock.CurrentValue.ToString().PadRight(16) +
+                    newStock.NumChanges.ToString().PadRight(16) + DateTime.Now.ToString().PadRight(16);
                 Console.WriteLine(output);
-                //overwrite file with titles once per loop
-                if (flag)
-                {
-                    using (StreamWriter writer = new StreamWriter(destPath))
-                    {
-                        writer.WriteLine(titles);
-                    }
-                    flag = false;
-                }
+                //add title to console and file
+                //addTitles();
                 //Display the output to the file
                 using (StreamWriter outputFile = new StreamWriter(destPath, true))
                 {
